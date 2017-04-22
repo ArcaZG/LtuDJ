ae147a651e2e4a95f0efaea399f0497ff8bb57b9
ArcaZG <japan998.lt.g@gmail.com>
2017-04-22 20:28:00 +0300

    Update LtuDJ.js

--- LtuDJ.js
+++ LtuDJ.js
@@ -242,15 +242,15 @@
         loggedInID: null,
         scriptLink: "https://rawgit.com/ArcaZG/LtuDJ/master/LtuDJ.js",
         cmdLink: "http://git.io/245Ppg",
-        chatLink: "https://rawgit.com/ArcaZG/LtuDJ/master/lang/en.json",
+        chatLink: "https://rawgit.com/ArcaZG/LtuDJ/master/lang/ltu.json",
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
         retrieveFromStorage: retrieveFromStorage,
         settings: {
             botName: "LtuDJ",
-            language: "english",
-            chatLink: "https://rawgit.com/ArcaZG/LtuDJ/master/lang/en.json",
+            language: "lietuviu",
+            chatLink: "https://rawgit.com/ArcaZG/LtuDJ/master/lang/ltu.json",
             scriptLink: "https://rawgit.com/ArcaZG/LtuDJ/master/LtuDJ.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
